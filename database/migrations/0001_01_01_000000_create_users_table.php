8d2ade9720ef3db7ea94ab9b6209a289857b242a
DudkinR <DudkinR@ukr.net>
2024-05-07 14:42:32 +0300

    data base 191

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -11,6 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
+                if (!Schema::hasTable('users')) {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
@@ -20,21 +21,24 @@ return new class extends Migration
             $table->rememberToken();
             $table->timestamps();
         });
-
+                }
+ if (!Schema::hasTable('password_reset_tokens')) {
         Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
-            $table->string('token');
+            $table->string('email', 191)->primary();
+            $table->string('token', 191);
             $table->timestamp('created_at')->nullable();
         });
-
+}
+ if (!Schema::hasTable('sessions')) {
         Schema::create('sessions', function (Blueprint $table) {
-            $table->string('id')->primary();
+            $table->string('id', 191)->primary();
             $table->foreignId('user_id')->nullable()->index();
             $table->string('ip_address', 45)->nullable();
             $table->text('user_agent')->nullable();
             $table->longText('payload');
             $table->integer('last_activity')->index();
         });
+    }
     }
 
     /**
