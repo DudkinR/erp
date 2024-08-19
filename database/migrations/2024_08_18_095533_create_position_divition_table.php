31224ddac0334c09de0c03067e8ddf25d73b3bf1
DudkinR <dudkinr@ukr.net>
2024-08-19 13:06:04 +0300

    division

--- database/migrations/2024_08_18_095533_create_position_divition_table.php
+++ database/migrations/2024_08_18_095533_create_position_divition_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        if (Schema::hasTable('personal_divition')) {
+        if (Schema::hasTable('personal_division')) {
             return;
         }
         Schema::create('personal_division', function (Blueprint $table) {
